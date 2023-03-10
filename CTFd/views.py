bea868d2fe619a088cfdc92dfb05398d5ef967c6
Himanshu Das <himanshudas75@gmail.com>
2023-03-10 16:30:20 +0530

    updated index.html

--- CTFd/views.py
+++ CTFd/views.py
@@ -170,17 +170,17 @@ def setup():
             # Splice in our banner
             index = f"""<div class="row">
     <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="{default_ctf_banner_location}" />
+        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="/themes/core/static/img/logo.png" />
         <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
+          <p>Welcome to Infosec CyberLabs</p>
             <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/company/cyberlabs-iitism/"><i class="fab fa-linkedin fa-2x" aria-hidden="true"></i></a>&nbsp;
+            <a href="https://www.instagram.com/cyberlabs_iitism/"><i class="fab fa-instagram fa-2x" aria-hidden="true"></i></a>&nbsp;
+            <a href="https://github.com/Cyber-Labs"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
         </h3>
         <br>
         <h4 class="text-center">
-            <a href="admin">Click here</a> to login and setup your CTF
+          <a href="https://cyberlabs.club">CyberLabs</a>
         </h4>
     </div>
 </div>"""
